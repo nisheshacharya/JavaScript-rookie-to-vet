928c6df5ba1bd88932e6a6f0bb6364156f24ab00
nisheshacharya <116048513+nisheshacharya@users.noreply.github.com>
2024-01-09 16:17:58 -0600

    data types

---
+++ Variables/DataType/dataTypes.js
@@ -0,0 +1,13 @@
+const string = "abc";
+const number = 123;
+const boolean = true;
+const isNull = null; //null
+const object = { a: 1, b: 2 }; //object
+
+const notAnum = "abc"/2 ; //NaN
+const bigInt = 10000n;  //bigInt
+
+//Also, some other data types are:
+//Undefined
+// NaN
+// null
