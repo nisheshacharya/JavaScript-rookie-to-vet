37b34b8bf29b06c9f6a2e86d7de10d4cd8b9318b
nisheshacharya <116048513+nisheshacharya@users.noreply.github.com>
2024-01-15 15:00:15 -0600

    added switch

---
+++ Switch/swich.js
@@ -0,0 +1,38 @@
+//In javaScript, switch is used to check conditions where there are many conditions.
+
+const date = new Date();
+const day = date.getDay();
+
+switch (day) {
+  case 1:
+    console.log("It's Monday today.");
+    break;
+
+  case 1:
+    console.log("It's Monday today.");
+    break;
+
+  case 2:
+    console.log("It's Tuesday today.");
+    break;
+
+  case 3:
+    console.log("It's Wednesday today.");
+    break;
+
+  case 4:
+    console.log("It's Thursday today.");
+    break;
+
+  case 1:
+    console.log("It's Friday today.");
+    break;
+
+  case 1:
+    console.log("It's Saturday today.");
+    break;
+
+  case 1:
+    console.log("It's Sunday today.");
+    break;
+}
