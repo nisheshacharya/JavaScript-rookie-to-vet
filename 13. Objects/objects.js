25c8ea1a0e1cf7ec305d3c7365792d129eb3ca82
nisheshacharya <116048513+nisheshacharya@users.noreply.github.com>
2024-01-20 17:38:51 -0600

    multi word key

--- 13. Objects/objects.js
+++ 13. Objects/objects.js
@@ -2,16 +2,24 @@
 
 //Example of object:
 
-
 let student = { name: "John", score: 83, pass: true };
+
+let myObj = { count: 1, type: "object", "is good": true }; //multi-word key
 
 //Getting values
 
-console.log(student.name);   // "John"
-console.log(student.score);    // 83
-
+console.log(student.name); // "John"
+console.log(student.score); // 83
 
 student.grade = "A";
 
+console.log(student); //{ name: "John", score: 83, pass: true, grade: A }
 
-console.log(student);    //{ name: "John", score: 83, pass: true, grade: A }
+//Deleting data;
+
+delete student.grade;
+console.log(student); //{ name: "John", score: 83, pass: true }
+
+//Multi-word key can not be accessed simply by using it's key. We need to use [] like in objects
+
+console.log(myObj["is good"]); // true
