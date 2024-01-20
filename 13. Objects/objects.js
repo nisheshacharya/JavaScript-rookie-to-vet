51d27efa69217c13e8737e7681616af754865433
nisheshacharya <116048513+nisheshacharya@users.noreply.github.com>
2024-01-20 17:31:03 -0600

    objects

---
+++ 13. Objects/objects.js
@@ -0,0 +1,17 @@
+// Object in JavaScript is a data-type that stores data in form of key-value pair.
+
+//Example of object:
+
+
+let student = { name: "John", score: 83, pass: true };
+
+//Getting values
+
+console.log(student.name);   // "John"
+console.log(student.score);    // 83
+
+
+student.grade = "A";
+
+
+console.log(student);    //{ name: "John", score: 83, pass: true, grade: A }
