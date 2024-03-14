f56fb164cbd50940363a4bd584a742f1332c69fd
nisheshacharya <116048513+nisheshacharya@users.noreply.github.com>
2024-03-14 15:48:25 -0500

    Abstraction

--- OOPInJavaScript/OOP-pillars.js
+++ OOPInJavaScript/OOP-pillars.js
@@ -1,12 +1,35 @@
 /*
 OOP  is built on four main pillars:
 
-1.Encapsulation
+1.Abstraction
 
-2. Inheritance
+2. Encapsulation
 
-3. Abstraction
+3. Inheritance
 
 4. Polymorphism
 
+(POLLY has ABS IN-CAP)
+
 */
+
+
+//  1. Abstraction: Abstraction is hiding unnecessary details and showing only essential elements 
+//     that an object needs to function.
+ 
+
+class Car{
+    constructor(model, color){
+        this.model = model;
+        this.color = color;
+    }
+   drive(){
+    console.log("Driving ", this.color , "colored ", this.model )
+   }
+}
+
+const toyota = new Car("corolla", "red");
+toyota.drive();
+
+// Here, to run function drive, we do not need to understand mechanism. 
+//  We just need to have model and color and the job will be done.
